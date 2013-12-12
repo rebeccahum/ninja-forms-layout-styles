ebb9f48c7e346fe964285fabbeb7243d9691f381
Kevin Stover <kstover@gmail.com>
2013-12-12 13:37:05 +0000

    Admin scripts should now load the min or dev versions based on the NINJA_FORMS_JS_DEBUG constant.

--- includes/admin/scripts.php
+++ includes/admin/scripts.php
@@ -3,26 +3,19 @@
 add_action( 'admin_init', 'ninja_forms_style_admin_js' );
 function ninja_forms_style_admin_js(){
 	global $wp_version;
+
+	if ( defined( 'NINJA_FORMS_JS_DEBUG' ) && NINJA_FORMS_JS_DEBUG ) {
+		$suffix = '';
+		$src = 'dev';
+	} else {
+		$suffix = '.min';
+		$src = 'min';
+	}
+
 	if( isset( $_REQUEST['page'] ) AND ( $_REQUEST['page'] == 'ninja-forms-style' OR ( $_REQUEST['page'] == 'ninja-forms' AND ( isset( $_REQUEST['tab'] ) AND $_REQUEST['tab'] == 'form_layout' ) ) ) ){
-		if( version_compare( $wp_version, '3.5', '<' ) ){
-
-			wp_enqueue_script( 'iris',
-				NINJA_FORMS_STYLE_URL .'/js/min/iris.min.js', array( 'jquery-ui-draggable', 'jquery-ui-slider', 'jquery-touch-punch' ) );
-
-			wp_enqueue_script( 'wp-color-picker',
-				NINJA_FORMS_STYLE_URL .'/js/min/color-picker.min.js' );
-
-			wp_localize_script( 'wp-color-picker', 'wpColorPickerL10n', array(
-				'clear' => __( 'Clear', 'ninja-forms-style' ),
-				'defaultString' => __( 'Default', 'ninja-forms-style' ),
-				'pick' => __( 'Select Color', 'ninja-forms-style' ),
-				'current' => __( 'Current Color', 'ninja-forms-style' ),
-				) );
-		}else{
-			wp_enqueue_script( 'wp-color-picker' );
-		}
+		wp_enqueue_script( 'wp-color-picker' );
 		wp_enqueue_script( 'ninja-forms-style-admin',
-			NINJA_FORMS_STYLE_URL .'/js/min/ninja-forms-style-admin.min.js',
+			NINJA_FORMS_STYLE_URL .'/js/' . $src .'/ninja-forms-style-admin' . $suffix . '.js',
 			array( 'jquery', 'jquery-ui-dialog', 'jquery-ui-sortable' ) );
 	}
 }
