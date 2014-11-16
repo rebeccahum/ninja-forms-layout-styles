321b119212e4bec446562875ca2935e788f6e1b9
Kevin Stover <kstover@gmail.com>
2014-11-16 16:04:58 -0500

    Updating internationalisation.

--- ninja-forms-style.php
+++ ninja-forms-style.php
@@ -7,7 +7,7 @@ Version: 1.2
 Author: The WP Ninjas
 Author URI: http://ninjaforms.com
 Text Domain: ninja-forms-style
-Domain Path: /languages/
+Domain Path: /lang/
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -40,47 +40,38 @@ add_action( 'admin_init', 'ninja_forms_style_setup_license' );
  */
 function ninja_forms_style_load_translations() {
 
-	/** Set our unique textdomain string */
-	$textdomain = 'ninja-forms-style';
+  /** Set our unique textdomain string */
+  $textdomain = 'ninja-forms-style';
 
-	/** The 'plugin_locale' filter is also used by default in load_plugin_textdomain() */
-	$locale = apply_filters( 'plugin_locale', get_locale(), $textdomain );
+  /** The 'plugin_locale' filter is also used by default in load_plugin_textdomain() */
+  $locale = apply_filters( 'plugin_locale', get_locale(), $textdomain );
 
-	/** Set filter for WordPress languages directory */
-	$wp_lang_dir = apply_filters(
-		'ninja_forms_style_wp_lang_dir',
-		trailingslashit( WP_LANG_DIR ) . 'ninja-forms-style/' . $textdomain . '-' . $locale . '.mo'
-	);
+  /** Set filter for WordPress languages directory */
+  $wp_lang_dir = apply_filters(
+    'ninja_forms_style_wp_lang_dir',
+    trailingslashit( WP_LANG_DIR ) . 'ninja-forms-style/' . $textdomain . '-' . $locale . '.mo'
+  );
 
-	/** Translations: First, look in WordPress' "languages" folder = custom & update-secure! */
-	load_textdomain( $textdomain, $wp_lang_dir );
+  /** Translations: First, look in WordPress' "languages" folder = custom & update-secure! */
+  load_textdomain( $textdomain, $wp_lang_dir );
 
-	/** Translations: Secondly, look in plugin's "lang" folder = default */
-	$plugin_dir = trailingslashit( basename( dirname( __FILE__ ) ) );
-	$lang_dir = apply_filters( 'ninja_forms_style_lang_dir', $plugin_dir . 'languages/' );
-	load_plugin_textdomain( $textdomain, FALSE, $lang_dir );
+  /** Translations: Secondly, look in plugin's "lang" folder = default */
+  $plugin_dir = trailingslashit( basename( dirname( __FILE__ ) ) );
+  $lang_dir = apply_filters( 'ninja_forms_style_lang_dir', $plugin_dir . 'lang/' );
+  load_plugin_textdomain( $textdomain, FALSE, $lang_dir );
 
 }
 add_action( 'plugins_loaded', 'ninja_forms_style_load_translations' );
 
 
-global $wpdb;
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/admin.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/functions.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/form-settings/form-settings.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/field-settings/field-settings.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/field-type-settings/field-type-settings.php");
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/field-type-settings/sidebars/select-field.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/error-settings/error-settings.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/datepicker-settings/datepicker-settings.php");
-
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms-style/tabs/multipart-settings/multipart-settings.php");
 
 require_once(NINJA_FORMS_STYLE_DIR."/includes/admin/pages/ninja-forms/tabs/form-layout/form-layout.php");
