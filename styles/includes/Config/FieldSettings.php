0f7f216c6c8df2349e89e92204433d620f8818e8
Kyle B. Johnson <kyle@wpninjas.com>
2016-06-02 18:46:19 -0400

    add field setting fieldsets.

--- styles/includes/Config/FieldSettings.php
+++ styles/includes/Config/FieldSettings.php
@@ -7,6 +7,20 @@ return array(
         'type' => 'fieldset',
         'label' => __( 'Wrap Styles', 'ninja-forms' ),
         'width' => 'full',
-    )
+    ),
+
+    'label_styles' => array(
+        'name' => 'label_styles',
+        'type' => 'fieldset',
+        'label' => __( 'Label Styles', 'ninja-forms' ),
+        'width' => 'full',
+    ),
+
+    'element_styles' => array(
+        'name' => 'element_styles',
+        'type' => 'fieldset',
+        'label' => __( 'Element Styles', 'ninja-forms' ),
+        'width' => 'full',
+    ),
 
 );
