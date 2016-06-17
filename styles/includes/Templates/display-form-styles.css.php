fced4e9ed3c855bf84363f09b792361e747808f7
Kyle B. Johnson <kyle@wpninjas.com>
2016-06-17 11:49:54 -0400

    update css output for advacned css settings.

--- styles/includes/Templates/display-form-styles.css.php
+++ styles/includes/Templates/display-form-styles.css.php
@@ -7,7 +7,12 @@
     <?php foreach( $styles as $selector => $rules ): ?>
     <?php echo $selector; ?> {
         <?php foreach( $rules as $rule => $value ): ?>
-        <?php echo apply_filters( 'ninja_forms_styles_output_rule_' . $rule, $rule ); ?>:<?php echo $value; ?>;
+        <?php if( 'show_advanced_css' == $rule ) continue; ?>
+        <?php if( 'advanced' != $rule ): ?>
+            <?php echo apply_filters( 'ninja_forms_styles_output_rule_' . $rule, $rule ); ?>:<?php echo $value; ?>;
+        <?php else: ?>
+            <?php echo $value; ?>
+        <?php endif; ?>
         <?php endforeach; ?>
     }
     <?php endforeach; ?>
