711478ad9a5169020bdeb7f58ab5e6f240a6e062
Kyle B. Johnson <kyle@wpninjas.com>
2016-06-20 15:19:23 -0400

    version bump to v3.0.0

--- ninja-forms-style.php
+++ ninja-forms-style.php
@@ -4,7 +4,7 @@
  * Plugin Name: Ninja Forms - Layout & Styles
  * Plugin URI: https://ninjaforms.com/extensions/layout-styles/
  * Description: Form layout and styling add-on for Ninja Forms.
- * Version: 3.0.1
+ * Version: 3.0.0
  * Author: The WP Ninjas
  * Author URI: http://ninjaforms.com
  * Text Domain: ninja-forms-layout-styles
