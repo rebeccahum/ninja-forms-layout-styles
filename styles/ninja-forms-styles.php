aaafea1140b22935e0f87838fd08bbce59397c5c
Kevin Stover <kstover@gmail.com>
2017-05-02 08:22:58 -0400

    Updating to version 3.0.19.

--- styles/ninja-forms-styles.php
+++ styles/ninja-forms-styles.php
@@ -7,7 +7,7 @@ if( class_exists( 'NF_Styles', false ) ) return;
  */
 final class NF_Styles
 {
-    const VERSION = '3.0.5';
+    const VERSION = '3.0.19';
     const SLUG    = 'styles';
     const NAME    = 'Styles';
     const AUTHOR  = 'WP Ninjas';
